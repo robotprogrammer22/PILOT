bc9ea83e5de2b473e257e2672cdc4308e1433866
Rebecca Leggett <roboticsgirl60@gmail.com>
2020-03-11 12:53:22 -0700

    added file for histogram functions

---
+++ tools/histogram.php
@@ -0,0 +1,4 @@
+// put new functions from model/histogram_helper here
+// need to correctly format the array, like the json file one
+// might need to create an object
+// which data source do I need to use?
