1173a11edad7ec116043f2c7bf66aee151532996
Rebecca Leggett <roboticsgirl60@gmail.com>
2020-03-13 15:46:49 -0700

    started function

--- tools/histogram.php
+++ tools/histogram.php
@@ -1,4 +1,17 @@
+<?php
+
+require_once(dirname(__FILE__) '/targets/metaHelper.php');
+require_once(dirname(__FILE__) '/keywords_metaHelper.php');
+
 // put new functions from model/histogram_helper here
 // need to correctly format the array, like the json file one
 // might need to create an object
 // which data source do I need to use?
+
+
+function getData()
+{
+
+}
+
+?>
